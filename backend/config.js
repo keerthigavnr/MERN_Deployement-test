b57e8b528470e2ade157a4bc3cab2814849503a1
KEERTHIGA V N R <111515088+keerthigavnr@users.noreply.github.com>
2024-08-25 22:10:30 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 3000;
+export const PORT = 3003;
 // export const mongoDBURL = "mongodb://127.0.0.1:27017/faculties";
 export const mongoDBURL="mongodb+srv://keerthigavnr:31012004@cluster0.e9qxpnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
